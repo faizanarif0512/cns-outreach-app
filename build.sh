c71c3a23330921dd65a404df3b05fa3818fa2531
Faizan Arif <muhammadfaizanarif12@gmail.com>
2026-09-01 20:51:33 +0530

    Auto-create initial admin superuser during build

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create initial admin user if none exists
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser('admin', 'admin@180dc.org', 'AdminPass123!')
+    print('Auto-created admin user: admin / AdminPass123!')
+"
+
